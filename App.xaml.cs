fa333d2ce9c0565772ff68cb17bdc14209cfc861
Paul B Colburn <paulcolburn@outlook.com>
2026-04-28 17:56:29 -0500

    added catch for unhandled exceptions

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,12 @@ namespace TicTacToe
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+#if !DEBUG
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+#endif
+        }
     }
 }
